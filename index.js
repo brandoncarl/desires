ab8e5efe4e5159c620bd4371d5ca4b308ab34ee2
Brandon Carl <brandon.j.carl@gmail.com>
2016-01-30 13:55:16 -0500

    index.js: adds initial functionality

---
+++ index.js
@@ -0,0 +1,42 @@
+
+/*
+
+  desires
+
+*/
+
+"use strict";
+
+var npm = require("npmwrap"),
+    path = require("path"),
+    cache = {};
+
+
+module.exports = function(name, options) {
+
+  var library,
+      location,
+      options = options || {};
+
+  // Return cache if available
+  if (cache[name]) return cache[name];
+
+  // Allow for alternative directories
+  location = name;
+  if (options.dir) {
+    options.dir = options.dir.replace("\/node_modules\/?$", "");
+    location = path.join(options.dir, "node_modules", location);
+  }
+
+  // Attempt to load, install if absent
+  try {
+    library = require(location)
+  } catch (err) {
+    npm.installSync(name, { save : true, cwd : options.dir });
+    library = require(location);
+  }
+
+  // Cache
+  return cache[name] = library;
+
+}
