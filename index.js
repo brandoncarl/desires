0155e88981486b9379673c3d2afe4726e7a8e02f
Brandon Carl <brandon.j.carl@gmail.com>
2016-02-01 23:29:42 -0500

    Extensive commenting

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-
-/*
+/**
 
   desires
+  Copyright 2016 Brandon Carl
+  MIT Licensed
 
-*/
+**/
 
 "use strict";
 
-var npm = require("npmwrap"),
-    path = require("path"),
-    cache = {};
+
+//
+//  Dependencies
+//
+
+var npm          = require("npmwrap"),
+    path         = require("path"),
+    cache        = {};
 
 
-module.exports = function(name, config) {
+
+
+/**
+
+  Proxies native "require" command. Synchronously installs package if missing.
+  If the directory option is provided (dir), desires appends node_modules
+  in event that it is missing.
+
+  @param {String} name Name of the module to require.
+  @param {Object} options Options include: { save, saveDev, dir }
+  @returns {Module}
+
+**/
+
+module.exports = function(name, options) {
 
   var library,
       location,
